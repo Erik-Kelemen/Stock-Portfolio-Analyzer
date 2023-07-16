a89b9c7a37c54fec97783c650897320914c57615
Erik-Kelemen <kelemen.erik@gmail.com>
2023-07-16 19:25:58 -0400

    dashboard changes and related

---
+++ DataSource/generate_trades.py
@@ -0,0 +1,52 @@
+import csv
+import random
+from datetime import datetime, timedelta
+import sys
+sys.path.append('..')
+import constants
+"""
+A script for generating a randomized, valid trades.csv file for use in the analyzer.
+A valid trades.csv is one that never tries to sell more shares of a ticker than it holds.
+"""
+
+def generate_dummy_trades():
+    # Define the date range
+    start_date = datetime(2022, 1, 1)
+    end_date = datetime(2022, 12, 31)
+
+    # Define the list of tickers
+    tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN', 'FB', "RY.TO", "SHOP.TO", "BNS.TO", "TD.TO", "ENB.TO"]
+    # Market holidays of 2022
+    market_holidays = set([datetime(2022, 1, 17), datetime(2022,2,21), datetime(2022,4,15), 
+                            datetime(2022, 5, 30), datetime(2022, 6, 20), datetime(2022, 7, 4), 
+                            datetime(2022,9,5), datetime(2022, 11, 24), datetime(2022, 11, 25), 
+                            datetime(2022, 12, 26)])
+    def is_trading_day(date):
+        return date.weekday() < 5 and date not in market_holidays
+
+    def trade_today():
+        threshold = 0.25 #10% probability of a trade today
+        return random.random() <= threshold
+
+    # Generate trades data
+    holdings = {tick: 0 for tick in tickers}
+    trades_data = []
+    current_date = start_date
+    while current_date <= end_date:
+        if is_trading_day(current_date):
+            for ticker in tickers:
+                if trade_today():
+                    qty = random.choice([-1, 1]) * random.randint(1, 10) * 10  # Randomly generate positive/negative multiples of 10
+                    qty = max(qty, -holdings[ticker])
+                    if qty != 0:
+                        holdings[ticker] += qty
+                        trades_data.append([current_date.strftime('%Y-%m-%d'), ticker, qty])
+        current_date += timedelta(days=1)
+
+    # Write trades data to CSV file
+    with open(constants.TRADES_FILE, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['date', 'ticker', 'qty'])
+        writer.writerows(trades_data)
+
+    print(f"Trades data generated and saved to {constants.TRADES_FILE}.")
--- constants.py
+++
@@ -1,7 +0,0 @@
-"""
-Global configuration constants
-"""
-
-DB_FILE = r"../data/SQLite.db"
-PRICES_FILE = r"../data/prices.csv"
-TRADES_FILE = r"../data/trades.csv"
