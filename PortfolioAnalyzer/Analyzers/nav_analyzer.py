3bdc70c359357624571a53cdbab8300aafddf4a3
Erik-Kelemen <kelemen.erik@gmail.com>
2023-07-23 19:40:21 -0400

    Added support for variance analysis

--- PortfolioAnalyzer/Analyzers/nav_analyzer.py
+++ PortfolioAnalyzer/Analyzers/nav_analyzer.py
@@ -2,67 +2,73 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator, DateFormatter
 from ..portfolio_analyzer import PortfolioAnalyzer
 from datetime import datetime
+
 class NAV_Analyzer(PortfolioAnalyzer):
+    def __init__(self, trades, prices):
+        super().__init__(trades, prices)
+        self.anlyzd = False
+        self.analyze()
     def analyze(self):
         """
         Computes the Net Asset Value (NAV) per day for the date range of trades.
         Should return a Series with 1 value for each time bucket.
         """
-        st.write("1. Net Asset Value (NAV)")
-        st.write(""" Assumptions:
-                1. Trades represent changes to the positions and are transferred into 
-                the portfolio before the market open. 
-                2. Portfolio NAV is the SUM market value of long positions – short positions.
-                3. Portfolio NAV is assumed to be $0 prior to the first trade date.
+        if(not self.anlyzd):
+            self.anlyzd = True
+            self.preprocess()
+            self.prefix_sum_trades()
+            self.NAV = self.prices * self.trades
+            self.NAV_by_ticker = self.NAV.sum(axis=0)
+            self.NAV_by_date = self.NAV.sum(axis=1)
+            self.NAV_by_date.index = pd.to_datetime(self.NAV_by_date.index)
+            self.NAV_daily_returns = self.NAV_by_date.pct_change()
+            # self.print()
+    
+    def display(self):
+        st.markdown("### 1. Net Asset Value (NAV)")
+        st.write("""Net Asset Value is the current value of the portfolio, calculated as the SUM market value of 
+                long positions - short positions. It is different from profits & losses, which include the cost
+                of purchasing the particular investment.
                 """)
-        # Transform into dates * tickers, where each cell is a price or quantity
-        self.prices = self.prices.pivot(index = 'date', columns = 'ticker', values = 'price_USD')
-        self.trades = self.trades.pivot(index = 'date', columns = 'ticker', values = 'qty').fillna(0)
-        
-        # Drop all price columns not in trades and reindex to align columns
-        common_columns = self.prices.columns.intersection(self.trades.columns)
-        self.prices = self.prices[common_columns].reindex(columns = self.trades.columns)
-
-        # Fill in missing dates in trades with 0's
-        self.trades = self.trades.reindex(self.prices.index, fill_value = 0)
-
-        # Prefix sum down the columns to get net trades, not deltas:
-        self.trades = self.trades.cumsum(axis = 0)
-        
-        # st.write(f"{self.prices.shape[0]} x {self.prices.shape[1]}")
-        # st.write(self.prices)
-        # st.write(f"{self.trades.shape[0]} x {self.trades.shape[1]}")
-        # st.write(self.trades)
-        
-        self.NAV = self.prices * self.trades
-        # st.write(self.NAV)
-        self.NAV_by_ticker = self.NAV.sum(axis=0)
-        self.NAV_by_date = self.NAV.sum(axis=1)
-        
-    def graph(self):
+        st.write(""" Assumptions:  
+                1. Trades represent changes to the positions and are transferred into 
+                the portfolio before the market open.   
+                2. Portfolio NAV is assumed to be $0 prior to the first trade date.  
+                """)
         st.write("Net Asset Value (NAV) across time:")
-        # st.write(self.NAV_by_date.columns)
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(12, 8))
+        
         plt.plot(self.NAV_by_date.index, self.NAV_by_date.values, marker='o', linestyle='-', markersize=5)
+
+        months_locator = MonthLocator(interval=1)
+        months_format = DateFormatter('%b %Y')
+        plt.gca().xaxis.set_major_locator(months_locator)
+        plt.gca().xaxis.set_major_formatter(months_format)
+
         plt.xlabel('Date')
         plt.ylabel('NAV')
         plt.title('Net Asset Value (NAV) Over Time')
-        # plt.grid(True)
         plt.tight_layout()
         st.pyplot(plt)
-
-
+        plt.clf()
         st.write("Net Asset Value (NAV) by ticker:")
-        # st.write(self.NAV_by_date.columns)
-        plt.figure(figsize=(10, 6))
+        plt.figure(figsize=(12, 8))
         plt.bar(self.NAV_by_ticker.index, self.NAV_by_ticker.values)
+
         plt.xlabel('Date')
         plt.ylabel('NAV')
         plt.title('Net Asset Value (NAV) by ticker')
-        # plt.grid(True)
         plt.tight_layout()
         st.pyplot(plt)
 
-    
+    def print(self):
+        """
+        For displaying backend dataframe to the user or debugging.
+        """
+        super().print()
+        st.write(self.NAV)
+        st.write(self.NAV_by_ticker)
+        st.write(self.NAV_by_date)
--- PortfolioAnalyzer/Analyzers/pnl_analyzer.py
+++ PortfolioAnalyzer/Analyzers/pnl_analyzer.py
@@ -3,12 +3,89 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from ..portfolio_analyzer import PortfolioAnalyzer
+from matplotlib.dates import MonthLocator, DateFormatter
 
 class PNL_Analyzer(PortfolioAnalyzer):
-    def analyze(self):
-        st.write("2. Profits & Losses (PnL) Analyzer")
+    def __init__(self, trades, prices):
+        super().__init__(trades, prices)
+        self.anlyzd = False
+        self.analyze()
         
-        pass
-    def graph(self):
-        st.write("Portfolio Profits & Losses across time:")
-        pass
+    def analyze(self):
+        if(not self.anlyzd):
+            self.anlyzd = True
+
+            # Step 1: Merge prices and trades dataframes
+            self.portfolio = pd.merge(self.prices, self.trades, on=['date', 'ticker'])
+            
+            # Step 2: Calculate the net quantity of each ticker at each date
+            self.portfolio['net_qty'] = self.portfolio.groupby(['ticker'])['qty'].cumsum()
+
+            # Step 3: Calculate the total value of each trade for each ticker/date
+            self.portfolio['trade_value'] = self.portfolio['price_USD'] * self.portfolio['qty']
+
+            # Step 4: Calculate the realized PnL
+            self.portfolio['realized_pnl'] = -self.portfolio.groupby(['ticker'])['trade_value'].cumsum().fillna(0)
+
+            # Step 5: Calculate the unrealized PnL for open positions
+            self.portfolio['unrealized_pnl'] = self.portfolio['net_qty'] * self.portfolio['price_USD']
+
+            # Step 6: Combine the realized and unrealized PnL to get the total PnL
+            self.portfolio['total_pnl'] = self.portfolio['realized_pnl'] + self.portfolio['unrealized_pnl']
+
+            self.tot_pnl = self.portfolio.groupby('date')['total_pnl'].sum().reset_index()
+
+            self.realized_pnl = self.portfolio.groupby('date')['realized_pnl'].sum().reset_index()
+            self.unrealized_pnl = self.portfolio.groupby('date')['unrealized_pnl'].sum().reset_index()
+            
+            self.tot_pnl['date'] = pd.to_datetime(self.tot_pnl['date'])
+            self.tot_pnl = self.tot_pnl.set_index('date')['total_pnl']
+
+            self.realized_pnl['date'] = pd.to_datetime(self.realized_pnl['date'])
+            self.realized_pnl = self.realized_pnl.set_index('date')['realized_pnl']
+
+            self.unrealized_pnl['date'] = pd.to_datetime(self.unrealized_pnl['date'])
+            self.unrealized_pnl = self.unrealized_pnl.set_index('date')['unrealized_pnl']
+
+            self.daily_returns = self.tot_pnl.pct_change()
+            # self.print()
+
+    def display(self):
+        st.markdown("### 2. Profits & Losses (PnL)")
+        st.write("""Profits & Losses reflect the difference between total revenue generated (profits) and total 
+                expenses incurred (losses) during that period. In this analyzer, it is treated by separating
+                PnL into realized and unrealized profits & losses.
+                """)
+        plt.figure(figsize=(12, 7))
+        plt.plot(self.tot_pnl.index, self.tot_pnl.values, marker='o', linestyle='-', markersize=5)
+        
+        months_locator = MonthLocator(interval=1)
+        months_format = DateFormatter('%b %Y')
+        plt.gca().xaxis.set_major_locator(months_locator)
+        plt.gca().xaxis.set_major_formatter(months_format)
+        plt.legend()
+        plt.xlabel('Date')
+        plt.ylabel('PnL')
+        plt.title('Portfolio Profits & Losses across time')
+        plt.tight_layout()
+        st.pyplot(plt)
+        
+        plt.clf()
+        
+        plt.plot(self.unrealized_pnl.index, self.unrealized_pnl.values, marker = 'd', linestyle = '-', markersize=5, label = 'Unrealized PnL')
+        plt.plot(self.realized_pnl.index, self.realized_pnl.values, marker = 's', linestyle = '-', markersize=5, label = 'Realized PnL')
+        plt.gca().xaxis.set_major_locator(months_locator)
+        plt.gca().xaxis.set_major_formatter(months_format)
+        plt.legend()
+        plt.xlabel('Date')
+        plt.ylabel('PnL')
+        plt.title('Portfolio Unrealized vs. Realized PnL across time')
+        plt.tight_layout()
+        st.pyplot(plt)
+
+    def print(self):
+        super().print()
+        st.write(self.portfolio)
+        st.write(self.tot_pnl)
+        st.write(self.realized_pnl)
+        st.write(self.unrealized_pnl)
