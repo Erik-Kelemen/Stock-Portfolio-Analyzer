bcf5c0889a51cd4f8c54c284e777658813d9167f
Erik-Kelemen <kelemen.erik@gmail.com>
2023-07-27 23:08:50 -0400

    Flexibility for benchmarking portfolio and scalability for analyzers

--- PortfolioAnalyzer/Analyzers/nav_analyzer.py
+++ PortfolioAnalyzer/Analyzers/nav_analyzer.py
@@ -9,31 +9,32 @@ from datetime import datetime
 class NAV_Analyzer(Analyzer):
     def __init__(self, trades, prices):
         super().__init__(trades, prices)
-        self.anlyzd = False
-        self.analyze()
+        # self.anlyzd = False
+        # self.analyze()
 
     def analyze(self):
         """
         Computes the Net Asset Value (NAV) per day for the date range of trades.
         Also computes the Cash-Adjusted Net Asset Value, which is equivalent to P&L.
         """
-        if(not self.anlyzd):
-            self.anlyzd = True
-            self.preprocess()
-            self.holdings = self.trades.cumsum(axis = 0)
+        self.preprocess()
+        self.holdings = self.trades.cumsum(axis = 0)
 
-            self.NAV = self.prices * self.holdings
-            self.NAV_by_ticker = self.NAV.sum(axis=0)
-            self.NAV_by_date = self.NAV.sum(axis=1)
-            self.NAV_by_date.index = pd.to_datetime(self.NAV_by_date.index)
-            
-            self.Cash_Flows = (self.prices * self.trades).sum(axis=1)
-            self.Cash_Flows.index = pd.to_datetime(self.Cash_Flows.index)
-            self.Cash_Holdings = self.Cash_Flows.cumsum()
-            self.Cash_Adjusted_NAV = self.NAV_by_date - self.Cash_Holdings
-            # self.print()
+        self.NAV = self.prices * self.holdings
+        self.NAV_by_ticker = self.NAV.iloc[-1]
+        self.NAV_by_date = self.NAV.sum(axis=1)
+        self.NAV_by_date.index = pd.to_datetime(self.NAV_by_date.index)
+        
+        self.Cash_Flows = (self.prices * self.trades).sum(axis=1)
+        self.Cash_Flows.index = pd.to_datetime(self.Cash_Flows.index)
+        self.Cash_Holdings = self.Cash_Flows.cumsum()
+        self.Cash_Adjusted_NAV = self.NAV_by_date - self.Cash_Holdings
+        # self.print()
     
-    def display(self):
+    def display(self, benchmark_nav):
+        """
+        Graphs of portfolio NAV by time and by ticker.
+        """
         st.markdown("### 1. Net Asset Value (NAV)")
         st.write("""Net Asset Value is the current value of the portfolio, calculated as the SUM market value of 
                 long positions - short positions. It is different from profits & losses, which include the cost
@@ -47,7 +48,8 @@ class NAV_Analyzer(Analyzer):
         st.write("Net Asset Value (NAV) across time:")
         plt.figure(figsize=(12, 8))
         
-        plt.plot(self.NAV_by_date.index, self.NAV_by_date.values, marker='o', linestyle='-', markersize=5)
+        plt.plot(self.NAV_by_date.index, self.NAV_by_date.values, marker='o', linestyle='-', markersize=5, label = 'User Portfolio')
+        plt.plot(self.NAV_by_date.index, benchmark_nav.NAV_by_date.values, marker='x', linestyle = '-', markersize=5, label='S&P 500')
 
         months_locator = MonthLocator(interval=1)
         months_format = DateFormatter('%b %Y')
@@ -57,18 +59,21 @@ class NAV_Analyzer(Analyzer):
         plt.xlabel('Date')
         plt.ylabel('NAV')
         plt.title('Net Asset Value (NAV) Over Time')
+        plt.legend()
         plt.tight_layout()
         st.pyplot(plt)
         plt.clf()
-        st.write("Net Asset Value (NAV) by ticker:")
+        st.write("Final Net Asset Value (NAV) by ticker:")
         plt.figure(figsize=(12, 8))
         plt.bar(self.NAV_by_ticker.index, self.NAV_by_ticker.values)
 
         plt.xlabel('Ticker')
         plt.ylabel('NAV')
-        plt.title('Net Asset Value (NAV) by ticker')
+        plt.title('User Portfolio: Final Net Asset Value (NAV) by ticker')
         plt.tight_layout()
         st.pyplot(plt)
+
+        plt.clf()
 
     def print(self):
         """
--- PortfolioAnalyzer/controller.py
+++ PortfolioAnalyzer/controller.py
@@ -3,30 +3,25 @@ import numpy as np
 from .Analyzers import nav_analyzer
 from .Analyzers import pnl_analyzer
 from .Analyzers import risk_analyzer
-from .Analyzers import frontier_analyzer
-
+from .analyzer import Analyzer 
 import streamlit as st
 
 class Controller:
-    def __init__(self, trades, prices, analyzers = None):
+    def __init__(self, trades, prices):
         st.write(f"Welcome to the analyzer!")
         self.trades = trades
         self.prices = prices
         
-        if len(self.prices['currency'].unique()) > 1:
-            self.standardize_currency()
-        if not analyzers:
-            nav_anlyzr = nav_analyzer.NAV_Analyzer(trades, prices)
-            pnl_anlyzr = pnl_analyzer.PNL_Analyzer(trades, prices)
-            risk_anlyzr = risk_analyzer.Risk_Analyzer(trades, prices, nav_anlyzr)
-            frontier_anlyzr = frontier_analyzer.Frontier_Analyzer(trades, prices)
-            analyzers = [nav_anlyzr,
-                         pnl_anlyzr,
-                         risk_anlyzr,
-                         frontier_anlyzr]
-        self.analyzers = analyzers
+        self.standardize_currency()
+
+        benchmark_anlyzr = Analyzer(trades, prices)
+        self.benchmark_trades = benchmark_anlyzr.create_dummy_trades()
         
-            
+        self.analyzers = [nav_analyzer.NAV_Analyzer,
+                        pnl_analyzer.PNL_Analyzer,
+                        risk_analyzer.Risk_Analyzer]
+        
+        
     def standardize_currency(self):
         """
         Standardize all stock prices to USD using the appropriate =X conversion rate in prices.
@@ -48,5 +43,15 @@ class Controller:
             
     def analyze(self):
         for analyzer in self.analyzers:
-            analyzer.analyze()
-            analyzer.display()
+            if analyzer == risk_analyzer.Risk_Analyzer:
+                anlyzr = analyzer(self.trades, self.prices, self.benchmark_trades)
+                anlyzr.analyze()
+                anlyzr.display()
+            else:
+                anlyzr = analyzer(self.trades, self.prices)
+                benchmark_anlyzr = analyzer(self.benchmark_trades, self.prices)
+                
+                anlyzr.analyze()
+                benchmark_anlyzr.analyze()
+
+                anlyzr.display(benchmark_anlyzr)
